270e0c7c06b17fb3ca0ec4b4378f2b6830af4af1
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-09 18:53:22 +0900

    글 목록 URLconf 작성

--- pylog/config/urls.py
+++ pylog/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from config.views import index
+from blog.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
+    path("posts/", post_list),
 ]
