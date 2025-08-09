2b3facba7dbaec3c90288ccc6aa034ba59e1725e
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-09 18:27:11 +0900

    Comment admin 페이지 등록

--- pylog/blog/admin.py
+++ pylog/blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
