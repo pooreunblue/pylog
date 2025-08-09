d084d16693b1ddf94274aeb786514bd57f51c450
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-09 18:58:27 +0900

    Feat: 모든 글 목록 템플릿에 전달

--- pylog/blog/views.py
+++ pylog/blog/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from blog.models import Post
 
 def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        "posts":posts,
+    }
     return render(request, "post_list.html", context)
