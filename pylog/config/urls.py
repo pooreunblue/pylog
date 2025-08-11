daa3b5bc08928bfb9c4e98d4b99cff687cfc77f4
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-11 22:36:20 +0900

    Feat: 상세페이지 url 설정

--- pylog/config/urls.py
+++ pylog/config/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 from django.urls import path
 
 from config.views import index
-from blog.views import post_list
+from blog.views import post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
     path("posts/", post_list),
+    path("posts/1/", post_detail),
 ]
 urlpatterns += static(
     # URL의 접두어가 MEDIA_URL일 때는 정적파일을 돌려준다
