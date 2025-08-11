050ffba4591375b777d012c1a1a2fad021d6da12
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-11 22:18:28 +0900

    Feat: MEDIA_URL과 업로드 파일 연결

--- pylog/config/urls.py
+++ pylog/config/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
 from config.views import index
 from blog.views import post_list
 
@@ -8,3 +11,9 @@ urlpatterns = [
     path("", index),
     path("posts/", post_list),
 ]
+urlpatterns += static(
+    # URL의 접두어가 MEDIA_URL일 때는 정적파일을 돌려준다
+    prefix=settings.MEDIA_URL,
+    # 돌려줄 디렉터리는 MEDIA_ROOT를 기준으로 한다
+    document_root=settings.MEDIA_ROOT,
+)
