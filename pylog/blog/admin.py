83fd815426eb304a1ba638d0691dd090d2273bf4
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-11 22:21:57 +0900

    Feat: admin 썸네일 이미지 필드 추가

--- pylog/blog/admin.py
+++ pylog/blog/admin.py
@@ -3,6 +3,7 @@ from blog.models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "thumbnail"]
     pass
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
