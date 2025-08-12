9e87411257f5931b16d4be7b8eb57a78c7ec6732
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-13 02:29:29 +0900

    Feat: 글 작성 View 생성

--- pylog/blog/views.py
+++ pylog/blog/views.py
@@ -16,3 +16,6 @@ def post_detail(request, post_id):
         "post": post,
     }
     return render(request, "post_detail.html", context)
+
+def post_add(request):
+    return render(request, "post_add.html")
