aaef4558989c90b5c8fec36fafc7d360fc97e372
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-13 03:01:29 +0900

    Feat: Post 요청일 때 변수 할당

--- pylog/blog/views.py
+++ pylog/blog/views.py
@@ -18,4 +18,9 @@ def post_detail(request, post_id):
     return render(request, "post_detail.html", context)
 
 def post_add(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+    else:
+        ...
     return render(request, "post_add.html")
